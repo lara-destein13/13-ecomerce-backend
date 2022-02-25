2221d1c30a910f5ab9fec13d16f91deae21925d1
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-02-24 20:38:18 -0500

    when getting categories, be sure to include assiciated products

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,7 +6,14 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll()
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ],
+  })
     .then(dbCategoryData => res.json(dbCategoryData))
     .catch(err => {
       console.log(err);
@@ -20,7 +27,13 @@ router.get('/:id', (req, res) => {
   Category.findOne({
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ],
   })
     .then(dbCategoryData => {
       if (!dbCategoryData) {
